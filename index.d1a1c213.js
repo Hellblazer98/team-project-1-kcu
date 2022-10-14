37201fac503345d879da5302fde14b93bfe74e06
Hellblazer98 <99325740+Hellblazer98@users.noreply.github.com>
2022-10-14 18:20:51 +0000

    Deploying to gh-pages from @ Hellblazer98/team-project-1-kcu@f9a8d3e1f94c71e49860b130af366fd6f0dbfc14 🚀

---
+++ index.d1a1c213.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{const a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.d1a1c213.js.map
---
+++ index.de619005.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.de619005.js.map
