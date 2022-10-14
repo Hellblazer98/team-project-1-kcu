34f962dd57de060e391b63e1ea8a05eeb1dd7872
Hellblazer98 <99325740+Hellblazer98@users.noreply.github.com>
2022-10-14 18:23:43 +0000

    Deploying to gh-pages from @ Hellblazer98/team-project-1-kcu@9407816590cce2c613c42cf848663018161443e6 🚀

--- index.de619005.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.de619005.js.map
