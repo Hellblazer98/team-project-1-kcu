42c1a69e51088936401c52eba5b7e429015ce06b
Hellblazer98 <99325740+Hellblazer98@users.noreply.github.com>
2022-10-16 09:27:05 +0000

    Deploying to gh-pages from @ Hellblazer98/team-project-1-kcu@af88907a6fe1b0edc3b3e6cb4b24af5aa91dc22b 🚀

---
+++ index.fcf54e21.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,slidesToShow:1,speed:1e3,Infinity:!0,autoplay:!0,autoplaySpeed:2e3,easing:"linear"})}));
+//# sourceMappingURL=index.fcf54e21.js.map
